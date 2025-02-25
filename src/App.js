8560f8e818b7b1ba70f514ff1a63b53d8a623c6b
Aracely0504 <aracelybc90@gmail.com>
2025-02-24 21:14:44 -0800

    Added search engine component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>
+            This project was coded by{" "}
+            <a href="https://github.com/Aracely0504">Aracely Campos,</a> is{" "}
+            <a href="https://github.com/Aracely0504/dictionary-project">
+              {" "}
+              open-sourced on Github
+            </a>{" "}
+            and
+            <a href="https://dictionary-app-shecodesio.netlify.app/">
+              {" "}
+              hosted on Netlify.
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
